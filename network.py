dcac8f921afbc4f6eaf56ef7fa82ce53b0f2c305
PeterChain <pedroc25@msn.com>
2018-01-18 22:45:43 +0000

    First commit

---
+++ network.py
@@ -0,0 +1,10 @@
+"""
+Network.py
+File containing a simple script which receives a list of values and another list of of weights, 
+it creates a set of list of adjusted weights , it runs based on a fixed number of iterations and
+learning rate. manipulating these values will produce different set of results
+"""
+
+
+def adjust_learning(weights, values):
+    pass
